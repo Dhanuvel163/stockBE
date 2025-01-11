2160560877a801158fb0396b59dba91062ffe0e2
Dhanavel <dhanuram99@gmail.com>
2025-01-12 00:21:28 +0530

    Fix

--- controllers/OrganizationController.js
+++ controllers/OrganizationController.js
@@ -66,7 +66,8 @@ exports.organizationLogin = async (req, res, next) => {
         const token = jwt.sign({organization: organization}, SECRET, {expiresIn: '7d'});
         return res.status(200).json({
             success: true,
-            token: token
+            token: token,
+            organization
         })
     } catch (error) {
         handleError(error,res)
