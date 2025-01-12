a2455a4aad810aaaf45a701cab7ded0a868e5733
Dhanavel <dhanuram99@gmail.com>
2025-01-13 00:15:22 +0530

    Fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,8 @@ module.exports = function(req, res, next) {
   if (token) {
     try {
         const decoded = jwt.verify(token, SECRET)
-        req.decoded = decoded;
+        req.decoded = decoded?.organization;
+        req.decoded.id = decoded?.organization._id;
         next();
     } catch (error) {
         return res.status(403).json({
