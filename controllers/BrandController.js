ef4902e11633230db01d10de6bb0ce13256df948
Dhanavel <dhanuram99@gmail.com>
2025-01-21 02:50:44 +0530

    FIx

--- controllers/BrandController.js
+++ controllers/BrandController.js
@@ -20,6 +20,8 @@ exports.updateBrand = async (req, res, next) => {
     const {id:organization} = req.decoded
     const {name} = req.body
     try {
+        const brandWithName = await Brand.findOne({name,organization,_id:{$ne:id}});
+        if(brandWithName) throw {is_error: true, code: 400, message: "Brand name already present"}
         const brand = await Brand.findOne({_id:id,organization});
         if (!brand) throw {is_error: true, code: 404, message: "Brand not found"}
         brand.set({name});
@@ -37,6 +39,8 @@ exports.addBrand = async (req, res, next) => {
     try {
         const {name} = req.body;
         const {id} = req.decoded;
+        const brandWithName = await Brand.findOne({name,organization:id});
+        if(brandWithName) throw {is_error: true, code: 400, message: "Brand already present"}
         const brand = await Brand.create({name,organization:id});
         return res.status(201).json({
             success: true,
